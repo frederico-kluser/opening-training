b5bfcb62ef3ac53289f819e60c280267dff733ca
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2024-11-23 12:52:47 -0300

    feat: Simplificação do código do jogo de xadrez
    O código do jogo de xadrez foi simplificado. As funções foram convertidas para arrow functions e os comentários desnecessários foram removidos. Além disso, a lógica de movimentação das peças foi otimizada para melhorar a performance e a legibilidade do código.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 import { Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import Gap from './components/Gap';
-import { Chess, Move } from 'chess.js'; // Certifique-se de usar a importação correta
+import { Chess, Move } from 'chess.js';
 import { Chessboard } from 'react-chessboard';
 
 import './App.css';
 
 function App() {
 	const [invertedBoard, setInvertedBoard] = useState(false);
-	const [game, setGame] = useState(new Chess()); // Instância do jogo
+	const [game, setGame] = useState(new Chess());
 	const [history, setHistory] = useState<Move[]>([]);
 	const [actualPosition, setActualPosition] = useState<number>(-1);
 
@@ -18,33 +18,31 @@ function App() {
 		console.log('actualPosition :', actualPosition);
 	}, [history]);
 
-	// Função para processar o movimento
-	function makeAMove(move: any) {
-		const gameCopy = new Chess(); // Cria uma nova instância
-		gameCopy.load(game.fen()); // Carrega o estado atual do jogo na nova instância
+	const makeAMove = (move: any) => {
+		const gameCopy = new Chess();
+		gameCopy.load(game.fen());
 
-		const result = gameCopy.move(move); // Tenta realizar o movimento
-		if (result) setGame(gameCopy); // Atualiza o estado do jogo apenas se o movimento for válido
+		const result = gameCopy.move(move);
+		if (result) setGame(gameCopy);
 
-		return result; // Retorna null se o movimento foi ilegal, ou o objeto do movimento se for válido
-	}
+		return result;
+	};
 
-	// Callback para quando uma peça for solta
-	function onDrop(sourceSquare: any, targetSquare: any) {
+	const onDrop = (sourceSquare: string, targetSquare: string) => {
 		const move = makeAMove({
 			from: sourceSquare,
 			to: targetSquare,
-			promotion: 'q', // Sempre promove para dama por simplicidade
+			promotion: 'q',
 		});
 
-		// Se o movimento for inválido, retorne `false`
 		if (move === null) return false;
 
+		// TODO: preciso melhorar aqui, pra quando eu voltar com Undo e fizer outro movimento, não continue incrementando no history a reveria
 		setActualPosition(history.length);
 		setHistory([...history, move]);
 
-		return true; // Permite o movimento válido
-	}
+		return true;
+	};
 
 	const handleNavigatePosition = (index: number) => {
 		const newActualPosition = actualPosition + index;
@@ -94,8 +92,8 @@ function App() {
 			<Chessboard
 				id="BasicBoard"
 				boardOrientation={invertedBoard ? 'black' : 'white'}
-				position={game.fen()} // Define a posição do tabuleiro baseada no estado atual
-				onPieceDrop={onDrop} // Callback para quando uma peça é solta
+				position={game.fen()}
+				onPieceDrop={onDrop}
 			/>
 		</Gap>
 	);
