4b0144d372e86f01d74b92662870600a83ead9a2
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2024-11-23 21:48:35 -0300

    feat: Adiciona opção de padding no componente Gap
    Este commit introduz uma nova propriedade 'padding' ao componente Gap. Agora é possível definir um espaçamento interno para este componente, além do já existente 'size'. Também foi criado o tipo 'TypeValidNumbers' para melhorar a legibilidade e manutenção dos tamanhos válidos.

--- src/components/Gap/index.tsx
+++ src/components/Gap/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 
+type TypeValidNumbers =
+	| 4
+	| 8
+	| 12
+	| 16
+	| 20
+	| 24
+	| 32
+	| 40
+	| 48
+	| 56
+	| 64
+	| 72
+	| 80
+	| 88
+	| 96
+	| 104
+	| 112
+	| 120
+	| 128
+	| 200;
+
 interface GapProps {
 	horizontal?: boolean;
-	size: 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 64 | 72 | 80 | 88 | 96 | 104 | 112 | 120 | 128 | 200;
+	size: TypeValidNumbers;
+	padding?: TypeValidNumbers;
 	children: React.ReactNode;
 }
 
-const Gap = ({ horizontal = false, size, children }: GapProps) => {
+const Gap = ({ horizontal = false, size, padding, children }: GapProps) => {
 	const gapStyle = {
 		display: horizontal ? 'flex' : 'grid',
 		gap: `${size}px`,
+		padding: padding ? `${padding}px` : 0,
 	};
 
 	return <div style={gapStyle}>{children}</div>;
