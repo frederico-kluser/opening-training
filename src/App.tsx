8250ea8fbca7439bcc810592b3c04840e2ea9000
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2024-11-24 23:09:20 -0300

    refactor: Reestruturação do código e movimentação de lógica
    Realizamos uma grande reestruturação no código, movendo a lógica de tipos e estados relacionados ao jogo de xadrez do componente App para o componente Register. Agora, o estado é inicializado dentro do próprio componente Register, melhorando a organização e legibilidade do código. Além disso, adicionamos um novo prop 'variant' ao componente Register para permitir maior flexibilidade na manipulação dos dados do jogo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import ChessGame from './components/ChessGame';
 import './App.css';
 import { useState } from 'react';
 import Gap from './components/Gap';
@@ -6,33 +5,10 @@ import Upload from './components/Upload';
 import { Button } from 'react-bootstrap';
 import Register from './components/Register';
 
-type TypeMove = {
-	after: string; //  'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2';
-	before: string; // 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1';
-	color: string; //  'b';
-	flags: string; //  'b';
-	from: string; //   'e7';
-	lan: string; //    'e7e5';
-	piece: string; //  'p';
-	san: string; //    'e5';
-	to: string; //     'e5';
-};
-
-type TypeItem = {
-	move: TypeMove;
-	comment: string;
-	nextMoves: TypeItem[];
-};
-
-type TypeStorage = {
-	[variant: string]: TypeItem;
-};
-
 function App() {
-	const [storage, setStorage] = useState<TypeStorage>({});
 	const [variant, setVariant] = useState<string>('');
 
-	if (Object.keys(storage).length === 0 && !variant) {
+	if (!variant) {
 		return (
 			<Gap size={16} padding={32} centralize>
 				<Upload
@@ -53,7 +29,7 @@ function App() {
 		);
 	}
 
-	return <Register />;
+	return <Register variant={variant} />;
 }
 
 export default App;
--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,5 +1,5 @@
 import { Button, Form } from 'react-bootstrap';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Chess } from 'chess.js';
 import Gap from '../Gap';
 import { FaRedo, FaSave, FaUndo } from 'react-icons/fa';
@@ -7,9 +7,58 @@ import { RiFlipVerticalFill, RiFlipVerticalLine } from 'react-icons/ri';
 import { ImExit } from 'react-icons/im';
 import ChessGame from '../ChessGame';
 
-const Register = () => {
+type TypeMove = {
+	after: string; //  'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2';
+	before: string; // 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1';
+	color: string; //  'b';
+	flags: string; //  'b';
+	from: string; //   'e7';
+	lan: string; //    'e7e5';
+	piece: string; //  'p';
+	san: string; //    'e5';
+	to: string; //     'e5';
+};
+
+type TypeItem = {
+	move: TypeMove;
+	comment: string;
+	nextMoves: TypeItem[];
+};
+
+export type TypeStorage = {
+	[variant: string]: TypeItem;
+};
+
+interface RegisterProps {
+	variant: string;
+}
+
+const Register = ({ variant }: RegisterProps): JSX.Element => {
+	const [save, setSave] = useState<TypeStorage>({});
+
+	useEffect(() => {
+		setSave({
+			[variant]: {
+				move: {
+					after: '',
+					before: '',
+					color: '',
+					flags: '',
+					from: '',
+					lan: '',
+					piece: '',
+					san: '',
+					to: '',
+				},
+				comment: '',
+				nextMoves: [],
+			},
+		});
+	}, []);
+
 	const [invertedBoard, setInvertedBoard] = useState(false);
 	const [game, setGame] = useState(new Chess());
+	const [comment, setComment] = useState('');
 
 	const isBlackTurn = () => game.turn() === 'b';
 
@@ -60,7 +109,16 @@ const Register = () => {
 				>
 					Comentários
 				</Form.Label>
-				<Form.Control type="email" placeholder="Comente sobre a posição atual" as="textarea" rows={3} />
+				<Form.Control
+					type="email"
+					placeholder="Comente sobre a posição atual"
+					as="textarea"
+					rows={3}
+					value={comment}
+					onChange={(e) => {
+						setComment(e.target.value);
+					}}
+				/>
 			</Form>
 		</Gap>
 	);
