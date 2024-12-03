bffe6b41e799cc916b77c9c3cdd3110de871dd16
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2024-12-02 22:24:35 -0300

    feat: Melhoria na lógica do jogo de xadrez
    Aprimoramos a lógica do jogo de xadrez para melhorar a experiência do usuário. Agora, o estado inicial é definido e as jogadas são tratadas de maneira mais eficiente. Além disso, removemos alguns códigos redundantes e adicionamos comentários para facilitar o entendimento da lógica implementada.

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,26 +1,21 @@
 import { Button, Form } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import { Chess } from 'chess.js';
+import { Chess, Move } from 'chess.js';
 import Gap from '../Gap';
 import { FaRedo, FaSave, FaUndo } from 'react-icons/fa';
 import { RiFlipVerticalFill, RiFlipVerticalLine } from 'react-icons/ri';
 import { ImExit } from 'react-icons/im';
 import ChessGame from '../ChessGame';
 
-type TypeMove = {
-	after: string; //  'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2';
-	lan: string; //    'e7e5';
-	wasWhiteMove: boolean;
-};
-
-type TypeItem = {
-	move: TypeMove;
-	comment: string;
-};
+const initialFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
 
 export type TypeStorage = {
 	[variant: string]: {
-		[fen: string]: TypeItem[];
+		[fen: string]: {
+			prevFen: string[];
+			comment: string;
+			nextFen: string[];
+		};
 	};
 };
 
@@ -30,24 +25,62 @@ interface RegisterProps {
 
 const Register = ({ variant }: RegisterProps): JSX.Element => {
 	const [save, setSave] = useState<TypeStorage>({});
+	const [actualFen, setActualFen] = useState(initialFen);
 
 	const [invertedBoard, setInvertedBoard] = useState(false);
 	const [game, setGame] = useState(new Chess());
 	const [comment, setComment] = useState('');
-	const [actualMove, setActualMove] = useState<TypeMove[]>([]);
 
 	const isBlackTurn = () => game.turn() === 'b';
-
-	useEffect(() => {
-		console.log('actualMove :', actualMove);
-	}, [actualMove]);
 
 	useEffect(() => {
 		console.log('save :', save);
 	}, [save]);
 
+	useEffect(() => {
+		setSave((prevSave) => {
+			const newSave = { ...prevSave };
+			if (!newSave[variant]) {
+				newSave[variant] = {};
+			}
+			newSave[variant][actualFen] = {
+				prevFen: [],
+				comment,
+				nextFen: [],
+			};
+			return newSave;
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [comment, variant]);
+
+	useEffect(() => {
+		if (actualFen !== initialFen) {
+			setGame(new Chess(actualFen));
+		}
+	}, [actualFen]);
+
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const isNotMyTurn = () => (isBlackTurn() && !invertedBoard) || (!isBlackTurn() && invertedBoard);
+
+	const handleDrop = (move: Move) => {
+		const gameCopy = new Chess();
+		gameCopy.load(game.fen());
+
+		const newMove = gameCopy.move(move);
+		if (newMove) {
+			const fen = gameCopy.fen();
+			setSave((prevSave) => {
+				const newSave = { ...prevSave };
+				if (newSave[variant][actualFen].nextFen.indexOf(fen) === -1) {
+					newSave[variant][actualFen].nextFen.push(fen);
+				}
+				return newSave;
+			});
+			setActualFen(fen);
+		} else {
+			console.log('Invalid move');
+		}
+	};
 
 	return (
 		<Gap size={16} padding={16}>
@@ -74,53 +107,7 @@ const Register = ({ variant }: RegisterProps): JSX.Element => {
 				<option value="2">Two</option>
 				<option value="3">Three</option>
 			</Form.Select> */}
-			<ChessGame
-				invertedBoard={invertedBoard}
-				game={game}
-				onDropCallback={(move) => {
-					const gameCopy = new Chess();
-					gameCopy.load(game.fen());
-
-					const newMove = gameCopy.move(move);
-					if (newMove) {
-						const fen = gameCopy.fen();
-						const move = {
-							after: gameCopy.fen(),
-							lan: newMove.lan,
-							wasWhiteMove: gameCopy.turn() === 'w',
-						};
-
-						const saveCopy = { ...save };
-						if (!saveCopy[variant]) {
-							saveCopy[variant] = {};
-						}
-
-						if (!saveCopy[variant][fen]) {
-							saveCopy[variant][fen] = [];
-						} else if (!saveCopy[variant][fen].find((item) => item.move.lan === move.lan)) {
-							saveCopy[variant][fen].push({
-								move,
-								comment,
-							});
-						} else {
-							saveCopy[variant][fen] = saveCopy[variant][fen].map((item) => {
-								if (item.move.lan === move.lan) {
-									return {
-										...item,
-										comment,
-									};
-								}
-								return item;
-							});
-						}
-
-						setActualMove((prev) => [...prev, move]);
-						setSave(saveCopy);
-						setGame(gameCopy);
-						setComment('');
-					}
-				}}
-			/>
+			<ChessGame invertedBoard={invertedBoard} game={game} onDropCallback={handleDrop} />
 			<Form>
 				<Form.Label
 					style={{
